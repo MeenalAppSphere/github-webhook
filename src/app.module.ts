49a02570fd5822b73cf0158b89e25e70ac606a60
Meenal <meenal.solanki@appsphere.in>
2021-04-07 11:26:28 +0530

    modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [GithubWebhooksModule.forRoot({
-    webhookSecret: 'assignwork'
+    webhookSecret: ''
   })],
   controllers: [AppController, AuthController],
   providers: [AppService],
